feefe7484822b3ad57238bfc0b2c8fe48785e18e
kamch4tskiy <kamchatskiymma@gmail.com>
2023-11-19 17:08:42 +0300

    add python template

---
+++ template.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import sys
+import os
+import requests
+import pwn
+import re
+import json
+import base64
+import string
+
+host = sys.argv[1]
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+def main():
+    ...
+
+
+#===========================================================
+if __name__ == '__main__':
+    main()
